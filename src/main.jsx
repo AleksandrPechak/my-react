abb26eae548a37e500c4d2e60e4d410a5fee183b
AleksandrPechak <144905025+AleksandrPechak@users.noreply.github.com>
2024-03-24 17:46:36 +0200

    update 01-react

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// // Імпорт стилів нормалізації
 // import "modern-normalize";
 import App from "./components/app/App";
 import "./index.css";
