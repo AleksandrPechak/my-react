05093f75214eb0a2fc5b366cb973834cc777ab5e
AleksandrPechak <144905025+AleksandrPechak@users.noreply.github.com>
2024-03-25 16:12:09 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-// import "modern-normalize";
-import App from "./components/app/App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/app/App';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import 'modern-normalize';
+import './assets/common.scss';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+	<App />
+	</React.StrictMode>
 );
